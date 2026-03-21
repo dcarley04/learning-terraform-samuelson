94d81f88e8d17ba695b366c1a01a7c8813ead502
narrowwaymoto <dave@narrowwaymoto.com>
2026-03-21 14:37:35 -0700

    Refactor main.tf for region change and cleanup
    Updated AWS region and modified comments. Removed security group and EC2 instance resources.

--- main.tf
+++ main.tf
@@ -1,8 +1,8 @@
 provider "aws" {
-  region = "us-east-1"
+  region = "us-west-1"
 }
 
-# Free-tier Amazon Linux 2 AMI
+# Get latest Amazon Linux 2 AMI (free tier eligible)
 data "aws_ami" "amazon_linux" {
   most_recent = true
   owners      = ["amazon"]
@@ -11,43 +11,4 @@ data "aws_ami" "amazon_linux" {
     name   = "name"
     values = ["amzn2-ami-hvm-*-x86_64-gp2"]
   }
-}
-
-# Allow only HTTP
-resource "aws_security_group" "web_sg" {
-  name = "minimal-web-sg"
-
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
-
-# Single free-tier EC2 instance
-resource "aws_instance" "web" {
-  ami           = data.aws_ami.amazon_linux.id
-  instance_type = "t2.micro"
-
-  vpc_security_group_ids = [aws_security_group.web_sg.id]
-
-  user_data = <<EOF
-#!/bin/bash
-echo "hello world" > /var/www/html/index.html
-yum install -y httpd
-systemctl enable httpd
-systemctl start httpd
-EOF
-}
-
-output "url" {
-  value = "http://${aws_instance.web.public_ip}"
 }
