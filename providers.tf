c9ad68f6b0f5cf7a0eb03acf43d720b0b6092a83
narrowwaymoto <dave@narrowwaymoto.com>
2026-03-21 14:48:39 -0700

    Change AWS region from us-west-1 to us-east-1

--- providers.tf
+++ providers.tf
@@ -1,5 +1,15 @@
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+
+  required_version = ">= 1.3.0"
+}
+
 provider "aws" {
-  region = "us-west-1"
+  region = "us-east-1"
 }
 
 
