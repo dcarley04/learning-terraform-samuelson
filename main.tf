b12e0ca933d9108e796a416e2a7b891ae40abaf0
narrowwaymoto <dave@narrowwaymoto.com>
2026-03-21 13:40:42 -0700

    Refactor Terraform configuration for AWS resources
    Updated AWS provider configuration and changed AMI to Amazon Linux 2. Modified security group to allow HTTP and SSH access, and adjusted EC2 instance settings.

--- main.tf
+++ main.tf
@@ -1,24 +1,80 @@
-data "aws_ami" "app_ami" {
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+# Use the latest Amazon Linux 2 AMI (free-tier eligible)
+data "aws_ami" "amazon_linux" {
   most_recent = true
+  owners      = ["amazon"]
 
   filter {
     name   = "name"
-    values = ["bitnami-tomcat-*-x86_64-hvm-ebs-nami"]
+    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
   }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-
-  owners = ["979382823631"] # Bitnami
 }
 
+# Security group allowing HTTP + SSH
+resource "aws_security_group" "web_sg" {
+  name        = "webserver-sg"
+  description = "Allow HTTP and SSH"
+
+  ingress {
+    description = "HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# EC2 instance (free-tier eligible)
 resource "aws_instance" "web" {
-  ami           = data.aws_ami.app_ami.id
-  instance_type = var.instance_type
+  ami           = data.aws_ami.amazon_linux.id
+  instance_type = "t2.micro"
+
+  vpc_security_group_ids = [aws_security_group.web_sg.id]
+
+  user_data = <<-EOF
+              #!/bin/bash
+              echo "hello world" > /var/www/html/index.html
+              yum install -y httpd
+              systemctl enable httpd
+              systemctl start httpd
+              EOF
 
   tags = {
-    Name = "HelloWorld"
+    Name = "hello-world-webserver"
   }
+}
+
+output "public_ip" {
+  value = aws_instance.web.public_ip
+}
+
+output "website_url" {
+  value = "http://${aws_instance.web.public_ip}"
 }
