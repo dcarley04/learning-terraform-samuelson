ee80d16865690b166fc1b45197b88a6f509d4d35
Pratik Pattanaik <pratikpattanaik1992@outlook.com>
2026-04-22 08:47:46 +0530

    update security group tf

--- main.tf
+++ main.tf
@@ -65,7 +65,7 @@ type        = "egress"
 from_port   = 0
 to_port     = 0
 protocol    = -1
-cidr_blocks = ["0.0.0.0/0"]"
+cidr_blocks = ["0.0.0.0/0"]
 
 security_group_id = aws_security_group.blog.id
 }
