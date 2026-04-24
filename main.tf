b43f83523f87eba7b967a3140e7b6f45cc50d75e
Pratik Pattanaik <pratikpattanaik1992@outlook.com>
2026-04-24 09:02:37 +0530

    Create a DEV VPC

--- main.tf
+++ main.tf
@@ -17,7 +17,7 @@ data "aws_ami" "app_ami" {
 resource "aws_instance" "web" {
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
-
+  subnet_id = module.blog_vpc.public_subnets[0]
   vpc_security_group_ids = [module.blog_sg.security_group_id]
 
   tags = {
@@ -25,8 +25,22 @@ resource "aws_instance" "web" {
   }
 }
 
-data "aws_vpc" "default"{
-  default = true
+module "blog_vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = "dev"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["us-east-1a", "us-east-1b", "us-east-1c"]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+
+  enable_nat_gateway = true
+
+  tags = {
+    Terraform = "true"
+    Environment = "dev"
+  }
 }
 
 module "blog_sg" {
@@ -34,7 +48,7 @@ source  = "terraform-aws-modules/security-group/aws"
 version = "5.3.1"
 name   = "blog_new"
 
-vpc_id = data.aws_vpc.default.id
+vpc_id = module.blog_vpc.vpc_id
 
 ingress_rules = ["http-80-tcp","https-443-tcp"]
 ingress_cidr_blocks = ["0.0.0.0/0"]
